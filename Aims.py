e3d27655b3a648a404fa19e3ce630615901fe8ba
Kyouko2104 <g4brielalcantara@gmail.com>
2019-09-08 22:43:57 -0300

    aaaaa

--- Aims.py
+++ Aims.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from PIL import ImageTk,Image
 from json import load
+import tkinter.ttk
+import pygame
 
 Constants = load(open('utils/constants.json'))
 
@@ -12,16 +14,26 @@ def startMenu_open(self):
     window.iconbitmap(Constants['AIM_ICON'])
     window.title('Aims - Aprenda inglês de modo simples')
 
+
+
 class App:
 
 
 
+
     def __init__(self, parent):
-        
+
         self.parent = parent
 
         self.load_hud()
         self.load_images()
+
+        self.parent.tabs = tabs = tkinter.ttk.Notebook()
+        self.parent.tab01 = tab01 = tkinter.ttk.Frame(tabs)
+        self.parent.tabs.add = tabs.add(tab01, text='Aims')
+        self.parent.tabs.pack = tabs.pack(expand=1, fill='both')
+
+
 
         self.parent.brFlagLabel = Label(image=self.parent.BR_FLAG)
         self.parent.brFlagLabel.place(relx=0.5, rely=0.4, anchor=CENTER)
@@ -32,13 +44,15 @@ class App:
         self.parent.greatDayLabel = Label(text='Tenha um ótimo dia!!', bg='white', width=20)
         self.parent.greatDayLabel.pack(side=TOP)
 
-        self.parent.greatDayLabel =  Button(text='Aprender', width=20, bg='white', command=startMenu_open)
+        self.parent.greatDayLabel =  Button(text='Aprender', width=20, bg='white')
         self.parent.greatDayLabel.place(relx=0.5, rely=0.5, anchor=CENTER)
-        self.parent.greatDayLabel.bind('<Button-1>', startMenu_open)
+        self.parent.greatDayLabel.bind('<Button-1>')
 
         self.parent.quitButton  = Button(text='Fechar', bg='white')
         self.parent.quitButton.place(relx=0.0, rely=0.95)
         self.parent.quitButton.bind('<Button-1>', quit)
+
+
 
     def load_hud (self):
         self.parent.geometry('500x500+600+300')
@@ -49,6 +63,9 @@ class App:
     def load_images (self):
         self.parent.BR_FLAG = ImageTk.PhotoImage(Image.open(Constants['BR_FLAG']))
         self.parent.US_FLAG = ImageTk.PhotoImage(Image.open(Constants['US_FLAG_FIXED']))
+
+
+
 
 
 
--- Aprendizado.py
+++ Aprendizado.py
@@ -1,2 +1,8 @@
-a='atapoikkaawwaaqqee'.split()
-print(a)
+list = []
+
+list.append(len('ranana'))
+list.append(len('maçã'))
+list.append(len('pera'))
+list.append(len('primeiro valor'))
+list.sort()
+print(list)
